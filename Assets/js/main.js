66f1b2ebed003088564d00b63ff380db8e9ac9f8
Muhammad Faturahman Bancin <mufatraba@gmail.com>
2022-02-22 17:32:19 +0700

    initial commit ☄️

---
+++ Assets/js/main.js
@@ -0,0 +1,224 @@
+/*=============== LOADER ===============*/
+setTimeout(function () {
+  $('.loader__wrapper').fadeToggle();
+}, 3800);
+// $(window).on('load', function () {
+//   $('.loader__wrapper').fadeOut();
+// });
+
+// let spinnerWrapper = document.querySelector('.loader__wrapper');
+
+// window.addEventListener('load', function () {
+
+//   // spinnerWrapper.style.display = '';
+
+//   spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+// });
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+  mb.addEventListener('click', () =>{
+    modal(i)
+  })
+})
+
+modalClose.forEach((mc) =>{
+  mc.addEventListener('click',() =>{
+    modalViews.forEach((mv) =>{
+      mv.classList.remove('active-modal')
+    })
+  })
+})
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork(){
+  linkWork.forEach(l=> l.classList.remove('active-work'));
+  this.classList.add('active-work');
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork));
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
+
+/*=============== MEDIUM ZOOM ===============*/
+const imageZoom = document.querySelectorAll('.testimonial__img');
+
+  for(i = 0 ; i <= imageZoom.length; i++){
+      mediumZoom(imageZoom[i],{
+        background: '#5F6571',
+        margin: 8,
+        scrollOffset: 50,
+      })
+  }
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+      
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+    
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  //reset: true,
+
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`,{delay: 3400})
+sr.reveal(`.home__social, .home__scroll`,{delay: 3400, origin: 'bottom'})
+
+
+/*=============== SCROLL SMOOTH ===============*/
+let smoothScroll = new scrollToSmooth('nav a, .home__scroll', {
+  easing: 'easeInOutBack',
+  duration: 720,
+  durationRelative: true,
+  // onScrollStart: (data) => {
+  //   // console.log(data)
+  // },
+  // onScrollUpdate: (data) => {
+  //   // console.log(data)
+  // },
+  // onScrollEnd: (data) => {
+  //   // console.log(data)
+  // },
+});
+smoothScroll.init();
+
+/*=============== CURSOR ===============*/
+var cursor = document.querySelector('.cursor'),
+    cursorScale = document.querySelectorAll('.cursor__scale'),
+    mouseX = 0,
+    mouseY = 0
+
+gsap.to({}, 0.016,{
+  repeat: -1,
+
+  onRepeat: function(){
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY
+      }
+    })
+  }
+});
+
+window.addEventListener("mousemove", function(e){
+  mouseX = e.clientX;
+  mouseY = e.clientY
+});
+
+cursorScale.forEach(link =>{
+  link.addEventListener("mouseleave",() =>{
+    cursor.classList.remove('grow');
+    cursor.classList.remove('grow__small');
+  });
+  link.addEventListener("mousemove",() =>{
+    cursor.classList.add('grow');
+    if(link.classList.contains('small')){
+      cursor.classList.remove('grow');
+      cursor.classList.add('grow__small')
+    }
+  });
+});
+
